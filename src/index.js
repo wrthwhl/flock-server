ab3b947908bfd31f35532621a471c99636e899d6
Marco Kunz <kunz.marco@gmail.com>
2018-11-23 20:12:44 +0100

    chore (apollo): add apollo server index file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import config from '../config';
+import createServer from './graphql/';
+import * as models from './models';
+
+// Here, we are passing in our models as `context`.
+// Why is this helpful?
+// Using the models as context makes them automatically available
+// to all resolvers. (passed to them as the 3rd argument)
+// This way we have a neat separation between the `graphql`
+// and the `models` code.
+const server = createServer({ context: models });
+
+// Now, run the server 🔥
+server.listen({ port: config.PORT || 4000 }).then(({ url }) => {
+  // eslint-disable-next-line no-console
+  console.log(`🎡   GraphQL Playground ready at ${url}`);
+});
