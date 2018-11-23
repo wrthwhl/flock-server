4ea1ca63f3cd18fd28fc5699fea5afe7c287f148
Marco Kunz <kunz.marco@gmail.com>
2018-11-23 15:05:00 +0100

    chore (apollo): setup basic apollo server and file structure

---
+++ src/graphql/schema/index.js
@@ -0,0 +1,15 @@
+import merge from 'lodash.merge';
+
+// Root types
+import rootTypeDefs from './typeDefs.gql';
+import rootResolvers from './resolvers';
+
+// Entity types
+import { typeDefs as Author, resolvers as authorResolvers } from '../entities/User';
+
+export default {
+  // Apollo Server accepts an array of type definitions 👍
+  typeDefs  : [ rootTypeDefs, Author ],
+  // Since the resolvers are just objects, we can make due with a deep merge
+  resolvers : merge(rootResolvers, authorResolvers)
+};
