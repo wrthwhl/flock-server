9197c408db3dede57cac6e668b7b99b0ab7cc78e
Marco Kunz <kunz.marco@gmail.com>
2018-11-24 22:01:25 +0100

    test: add rudimentary test for schema validity

--- test/schema.test.js
+++ test/schema.test.js
@@ -1,19 +1,13 @@
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import { graphql } from 'graphql';
+import { makeExecutableSchema, mockServer } from 'graphql-tools';
 import typeDefs from '../src/graphql/schema';
 
 const schema = makeExecutableSchema(typeDefs);
 
-addMockFunctionsToSchema({ schema });
-
-const query = `
-query User {
-  User(id: 1) { id, name, email, firstName, lastName }
-}
-`;
-
-describe('sfasdfasdf', () => {
-  test('if this works', () => {
-    graphql(schema, query).then((result) => console.log('Got result', result));
+describe('Schemas', () => {
+  test('has valid type definitions', async () => {
+    expect(async () => {
+      const MockServer = mockServer(schema);
+      await MockServer.query('{ __schema { types { name } } }');
+    }).not.toThrow();
   });
 });
