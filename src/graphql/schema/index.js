f6d19d5afdab3a2fb63d35a9e9af50d61c24748f
Marco Kunz <kunz.marco@gmail.com>
2018-11-26 11:24:12 +0100

    refactor (Date): add date type

--- src/graphql/schema/index.js
+++ src/graphql/schema/index.js
@@ -4,12 +4,13 @@ import merge from 'lodash.merge';
 import rootTypeDefs from './root.typeDefs.gql';
 import rootResolvers from './root.resolvers';
 
+import { typeDefs as DateType, resolvers as DateResolvers } from './Date';
 import { typeDefs as User, resolvers as userResolvers } from './User';
 import { typeDefs as Trip, resolvers as tripResolvers } from './Trip';
 
 export default {
   // Apollo Server accepts an array of type definitions 👍
-  typeDefs: [ rootTypeDefs, User, Trip ],
+  typeDefs: [ rootTypeDefs, DateType, User, Trip ],
   // Since the resolvers are just objects, we can make do with a deep merge
-  resolvers: merge(rootResolvers, userResolvers, tripResolvers)
+  resolvers: merge(rootResolvers, DateResolvers, userResolvers, tripResolvers)
 };
