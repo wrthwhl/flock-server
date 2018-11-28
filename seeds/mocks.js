694a608938acaa8bae04691ce92e2042e8139529
Damien <damienderail@yahoo.com>
2018-11-28 16:18:47 +0100

    seed file

---
+++ seeds/mocks.js
@@ -0,0 +1,136 @@
+/* const data = {
+  trips        : [
+    {
+      name         : 'Graduation Trip',
+      participants : [ 0, 1, 2, 3, 4 ],
+      destination  : {
+        isDicatated       : false,
+        chosenDestination : 0,
+        suggestions       : [
+          {
+            name    : 'Grenoble',
+            voters  : [ 0 ],
+            creator : 0
+          },
+          {
+            name    : 'cologne',
+            voters  : [ 1 ],
+            creator : 1
+          },
+          {
+            name    : 'barcelona',
+            voters  : [ 2 ],
+            creator : 2
+          },
+          {
+            name    : 'saoPaulo',
+            voters  : [ 3 ],
+            creator : 3
+          },
+          {
+            name    : 'zurich',
+            voters  : [ 4 ],
+            creator : 4
+          }
+        ]
+      } */
+
+/* ,
+      budget       : {
+        chosenBudget : 500,
+        suggestions  : {
+          500  : {
+            voters  : [ 0 ],
+            creator : 0
+          },
+          600  : {
+            voters  : [ 1 ],
+            creator : 1
+          },
+          700  : {
+            voters  : [ 2 ],
+            creator : 2
+          },
+          1000 : {
+            voters  : [ 3 ],
+            creator : 3
+          },
+          400  : {
+            voters  : [ 4 ],
+            creator : 4
+          }
+        }
+      },
+      timeFrame    : {
+        chosenTimeFrame : null,
+        suggestions     : {
+          0 : {
+            startDate : '2018-12-16',
+            endDate   : '2018-12-23',
+            voters    : [ 0 ],
+            creator   : 0
+          },
+          1 : {
+            startDate : '2018-12-16',
+            endDate   : '2018-12-23',
+            voters    : [ 1 ],
+            creator   : 1
+          },
+          2 : {
+            startDate : '2018-12-16',
+            endDate   : '2018-12-23',
+            voters    : [ 2 ],
+            creator   : 2
+          },
+          3 : {
+            startDate : '2018-12-16',
+            endDate   : '2018-12-23',
+            voters    : [ 3 ],
+            creator   : 3
+          },
+          4 : {
+            startDate : '2018-12-16',
+            endDate   : '2018-12-23',
+            voters    : [ 4 ],
+            creator   : 4
+          }
+        }
+      }
+    }
+  ],
+  destinations : {
+    cologne   : {
+      name : 'Köln'
+    },
+    grenoble  : {
+      name : 'Grenoble'
+    },
+    barcelona : {
+      name : 'Barcelona'
+    },
+    saoPaulo  : {
+      name : 'São Paulo'
+    },
+    zurich    : {
+      name : 'Zurich'
+    }
+  }
+};
+
+/* const trips = (trips) => {
+  let res = {};
+  trips.forEach(
+    (trip, id) =>
+      (res[id] = {
+        id,
+        ...trip
+      })
+  );
+  return res;
+}; */
+/*
+module.exports = {
+  getUsers : getUsers(buildUsers(data.users)),
+  trips    : data.trips //        : trips(data.trips),
+  // destinations : data.destinations
+}; */
