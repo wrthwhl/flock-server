5e25bb52232affff835184d41b04e8334566699a
Damien <damienderail@yahoo.com>
2018-11-28 13:00:56 +0100

    change in mutation creation trip and resolver

--- src/graphql/schema/Trip/resolvers.js
+++ src/graphql/schema/Trip/resolvers.js
@@ -9,9 +9,28 @@ export default {
 
   Mutation          : {
     updateTrip : (_, { input: { ...update } }, { Trip }) => Trip.findOneAndUpdate(update),
-    createTrip : (_, {input}, { Trip }) => Trip.create(
-      input
-    )
+    createTrip : async (_, {input, userID}, { Trip }) => {
+      input.destination.suggestions = input.destination.suggestions.map(name => ({
+        name,
+        voters: [userID],
+        creator: userID
+      }));
+      input.budget.suggestions = input.budget.suggestions.map(value => ({
+        value,
+        voters: [userID],
+        creator: userID
+      }));
+      input.timeFrame.suggestions = input.timeFrame.suggestions.map(object => ({
+        ...object,
+        voters: [userID],
+        creator: userID
+      }));
+      const res = await Trip.create(
+        input
+      );
+    
+      return res;
+    }
   },
 
   Trip              : {
--- src/models/trip.model.js
+++ src/models/trip.model.js
@@ -44,52 +44,52 @@ const TripSchema = new Schema({
 
 const Trip = mongoose.model('trips', TripSchema);
 
-(async function() {
-  await Trip.deleteMany({});
-  await Trip.create({
-    name         : 'Graduation Trip',
-    participants : [
-      '5bfc4455e3833d7f2a7ff22b',
-      '5bfc4455e3833d7f2a7ff22a',
-      '5bfc4455e3833d7f2a7ff229',
-      '5bfc4455e3833d7f2a7ff228'
-    ],
-    destination  : {
-      isDictated        : false,
-      chosenDestination : null,
-      suggestions       : [
-        {
-          name    : 'Barcelona',
-          voters  : [ '5bfc4455e3833d7f2a7ff22b', '5bfc4455e3833d7f2a7ff22a' ],
-          creator : '5bfc4455e3833d7f2a7ff22b'
-        }
-      ]
-    },
-    budget       : {
-      isDictated        : false,
-      chosenBudget : null,
-      suggestions       : [
-        {
-          value   : 500,
-          voters  : [ '5bfc4455e3833d7f2a7ff22b', '5bfc4455e3833d7f2a7ff22a' ],
-          creator : '5bfc4455e3833d7f2a7ff22b'
-        }
-      ]
-    },
-    timeFrame    : {
-      isDictated        : true,
-      chosenTimeFrame : null,
-      suggestions       : [
-        {
-          startDate : '2018-12-16',
-          endDate   : '2018-12-23',
-          voters    : [ '5bfc4455e3833d7f2a7ff22b', '5bfc4455e3833d7f2a7ff22a' ],
-          creator   : '5bfc4455e3833d7f2a7ff22b'
-        }
-      ]
-    }
-  });
-  // const trips = await Trip.find();
-})();
+// (async function() {
+//   await Trip.deleteMany({});
+//   await Trip.create({
+//     name         : 'Graduation Trip',
+//     participants : [
+//       '5bfc4455e3833d7f2a7ff22b',
+//       '5bfc4455e3833d7f2a7ff22a',
+//       '5bfc4455e3833d7f2a7ff229',
+//       '5bfc4455e3833d7f2a7ff228'
+//     ],
+//     destination  : {
+//       isDictated        : false,
+//       chosenDestination : null,
+//       suggestions       : [
+//         {
+//           name    : 'Barcelona',
+//           voters  : [ '5bfc4455e3833d7f2a7ff22b', '5bfc4455e3833d7f2a7ff22a' ],
+//           creator : '5bfc4455e3833d7f2a7ff22b'
+//         }
+//       ]
+//     },
+//     budget       : {
+//       isDictated        : false,
+//       chosenBudget : null,
+//       suggestions       : [
+//         {
+//           value   : 500,
+//           voters  : [ '5bfc4455e3833d7f2a7ff22b', '5bfc4455e3833d7f2a7ff22a' ],
+//           creator : '5bfc4455e3833d7f2a7ff22b'
+//         }
+//       ]
+//     },
+//     timeFrame    : {
+//       isDictated        : true,
+//       chosenTimeFrame : null,
+//       suggestions       : [
+//         {
+//           startDate : '2018-12-16',
+//           endDate   : '2018-12-23',
+//           voters    : [ '5bfc4455e3833d7f2a7ff22b', '5bfc4455e3833d7f2a7ff22a' ],
+//           creator   : '5bfc4455e3833d7f2a7ff22b'
+//         }
+//       ]
+//     }
+//   });
+//   // const trips = await Trip.find();
+// })();
 
 export default Trip;
