7f4932cc346db3e52118e1aebf10d8e45b3f1074
Christopher Buecklein <christopher_b@gmx.de>
2018-11-30 17:04:27 +0100

    build: changed branch so subscriptions are working again

--- src/graphql/schema/User/resolvers.js
+++ src/graphql/schema/User/resolvers.js
@@ -1,25 +1,46 @@
+import bcrypt from 'bcrypt';
+import { getJWT } from '../resolver-helpers';
+import { AuthenticationError } from 'apollo-server';
+
 export default {
   Query: {
-    user: (_, { id }, { User }) => User.findOne({ _id: id }),
+    self: (_, __, { User, user: { email } }) => User.findOne({ email }),
     allUsers: (_, __, { User }) => User.find()
   },
 
   Mutation: {
-    updateUser: (_, { input: { ...update } }, { User }) => User.findOneAndUpdate(update),
-    createUser: (_, { input: { firstName, lastName, email, avatar_url } }, { User }) =>
-      User.create({
-        firstName,
-        lastName,
-        email,
-        avatar_url
-      })
+    updateUser: (_, { id, update }, { User }) =>
+      User.findOneAndUpdate({ _id: id }, update),
+    register: async (_, { email, password, user }, { User }) => {
+      password = await bcrypt.hash(password, 12);
+      try {
+        await User.findOneAndUpdate(
+          { email },
+          { email, password, ...user },
+          { upsert: true }
+        );
+      } catch (err) {
+        throw new Error('Couldn\'t create user');
+      }
+      return getJWT({ email });
+    },
+    login: async (_, { email, password }, { User }) => {
+      const user = await User.findOne({ email });
+      let valid = false;
+      if (user) {
+        valid = true === (await bcrypt.compare(password, user.password));
+      }
+      if (
+        process.env.ENV.toLowerCase().includes('dev') &&
+        password === 'YouFlock!'
+      )
+        valid = true; // TODO remove PASSEPARTOUT
+      if (!user || !valid) throw new AuthenticationError();
+      return await getJWT({ email: user.email });
+    }
   },
 
   User: {
     trips: ({ id }, _, { Trip }) => Trip.find({ participants: id })
   }
-
-  // User : {
-  //   trips : ({ id }, _, { Trip }) => Trip.find({ participants: id })
-  // }
 };
--- src/graphql/server.js
+++ src/graphql/server.js
@@ -1,14 +1,39 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, AuthenticationError } from 'apollo-server';
 import graphQlSchema from './schema';
+import jwt from 'jsonwebtoken';
+
+const getJWTPayload = async ({ req, res }, SECRET) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return {};
+  const [ key, token ] = authHeader.split(' ');
+  try {
+    if (!key || !token || key.toUpperCase() !== 'BEARER') return res.status('401');
+    const user = await jwt.verify(token, SECRET);
+    return user;
+  } catch (err) {
+    res.status('401');
+    throw new AuthenticationError(
+      'Request contains invalid authorization header. Bearer token with valid JWT expected.',
+      err
+    );
+  }
+};
 
 export default {
-  launch: (config = {}, port = 4000) => {
+  launch: (models, apolloConfig = {}, port = 4000, SECRET) => {
     const server = new ApolloServer({
       ...graphQlSchema,
-      ...config
+
+      ...apolloConfig,
+      context: async (ctx) => {
+        const user = await getJWTPayload(ctx, SECRET);
+        return { ...models, user };
+
+      }
     });
-    server.listen({ port }).then(({ url }) => {
-      console.log(`✔️  GraphQL up and running, playground ready at ${url}`); // eslint-disable-line no-console
-    });
+    server
+      .listen({ port })
+      .then(({ url }) => console.log(`✔️  GraphQL up and running, playground ready at ${url}`)) // eslint-disable-line no-console
+      .catch((err) => console.error(err)); // eslint-disable-line no-console
   }
 };
