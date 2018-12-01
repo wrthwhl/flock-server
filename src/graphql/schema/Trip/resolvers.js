cd998dddbe4e6b38e756549f7be30c274cf5e3ac
Damien <damienderail@yahoo.com>
2018-12-01 20:39:02 +0100

    remove participant mutation added and tested

--- src/graphql/schema/Trip/resolvers.js
+++ src/graphql/schema/Trip/resolvers.js
@@ -32,8 +32,13 @@ export default {
         async (payload, { tripID }, { User, user: { email } }) => {
           const user = await User.findOne({ email });
           const { tripInfoChanged: updatedTrip } = payload;
-          const participants = updatedTrip.participants.map((participant) => String(participant));
-          return participants.includes(user._id.toString()) && (!tripID || String(tripID) === String(updatedTrip._id));
+          const participants = updatedTrip.participants.map(participant =>
+            String(participant)
+          );
+          return (
+            participants.includes(user._id.toString()) &&
+            (!tripID || String(tripID) === String(updatedTrip._id))
+          );
         }
       )
     },
@@ -54,61 +59,135 @@ export default {
 
   Mutation: {
     createTrip: async (_, { trip }, { Trip, User, user }) => {
-      const { newTrip, affectedUsers } = await createTrip(trip, user, { Trip, User });
+      const { newTrip, affectedUsers } = await createTrip(trip, user, {
+        Trip,
+        User
+      });
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: newTrip });
       affectedUsers.forEach(({ id, allTrips }) => {
-        pubsub.publish('OWN_TRIPS_CHANGED', { ownTripsChanged: allTrips, affectedUser: id });
+        pubsub.publish('OWN_TRIPS_CHANGED', {
+          ownTripsChanged: allTrips,
+          affectedUser: id
+        });
       });
       return newTrip;
     },
     addParticipants: async (_, { tripID, participants }, { User, Trip }) => {
-      let { updatedTrip, affectedUsers } = await addParticipants(tripID, participants, { User, Trip });
+      let { updatedTrip, affectedUsers } = await addParticipants(
+        tripID,
+        participants,
+        { User, Trip }
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       affectedUsers.forEach(({ id, allTrips }) => {
-        pubsub.publish('OWN_TRIPS_CHANGED', { ownTripsChanged: allTrips, affectedUser: id });
+        pubsub.publish('OWN_TRIPS_CHANGED', {
+          ownTripsChanged: allTrips,
+          affectedUser: id
+        });
       });
       return updatedTrip;
     },
     removeParticipants: async (_, { tripID, participants }, { Trip, User }) => {
-      const { updatedTrip, affectedUsers } = await removeParticipants(tripID, participants, User, Trip);
+      const { updatedTrip, affectedUsers } = await removeParticipants(
+        tripID,
+        participants,
+        User,
+        Trip
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       affectedUsers.forEach(({ id, allTrips }) => {
-        pubsub.publish('OWN_TRIPS_CHANGED', { ownTripsChanged: allTrips, affectedUser: id });
+        pubsub.publish('OWN_TRIPS_CHANGED', {
+          ownTripsChanged: allTrips,
+          affectedUser: id
+        });
       });
       return updatedTrip;
     },
-    addOrVoteForDestination: async (_, { tripID, destinations }, { Trip, user }) => {
-      const updatedTrip = await addOrVoteForDestination(tripID, destinations, user, { Trip });
+    addOrVoteForDestination: async (
+      _,
+      { tripID, destinations },
+      { Trip, user }
+    ) => {
+      const updatedTrip = await addOrVoteForDestination(
+        tripID,
+        destinations,
+        user,
+        { Trip }
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
     addOrVoteForBudget: async (_, { tripID, budget }, { Trip, user }) => {
-      const updatedTrip = await addOrVoteForBudget(tripID, budget, user, { Trip });
+      const updatedTrip = await addOrVoteForBudget(tripID, budget, user, {
+        Trip
+      });
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
-    addOrVoteForTimeFrame: async (_, { tripID, timeFrames }, { Trip, user }) => {
-      const updatedTrip = await addOrVoteForTimeFrame(tripID, timeFrames, user, { Trip });
+    addOrVoteForTimeFrame: async (
+      _,
+      { tripID, timeFrames },
+      { Trip, user }
+    ) => {
+      const updatedTrip = await addOrVoteForTimeFrame(
+        tripID,
+        timeFrames,
+        user,
+        { Trip }
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
-    removeVoteForDestination: async (_, { tripID, suggestionID }, { Trip, user }) => {
-      const updatedTrip = await removeVoteForDestination(tripID, suggestionID, user, Trip);
+    removeVoteForDestination: async (
+      _,
+      { tripID, suggestionID },
+      { Trip, user }
+    ) => {
+      const updatedTrip = await removeVoteForDestination(
+        tripID,
+        suggestionID,
+        user,
+        Trip
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
-    removeVoteForTimeFrame: async (_, { tripID, suggestionID }, { Trip, user }) => {
-      const updatedTrip = await removeVoteForTimeFrame(tripID, suggestionID, user, Trip);
+    removeVoteForTimeFrame: async (
+      _,
+      { tripID, suggestionID },
+      { Trip, user }
+    ) => {
+      const updatedTrip = await removeVoteForTimeFrame(
+        tripID,
+        suggestionID,
+        user,
+        Trip
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
-    removeVoteForBudget: async (_, { tripID, suggestionID }, { Trip, user }) => {
-      const updatedTrip = await removeVoteForBudget(tripID, suggestionID, user, Trip);
+    removeVoteForBudget: async (
+      _,
+      { tripID, suggestionID },
+      { Trip, user }
+    ) => {
+      const updatedTrip = await removeVoteForBudget(
+        tripID,
+        suggestionID,
+        user,
+        Trip
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
+
     lockDestination: async (_, { tripID, suggestionID }, { Trip, user }) => {
-      const updatedTrip = await lockDestination(tripID, suggestionID, user, Trip);
+      const updatedTrip = await lockDestination(
+        tripID,
+        suggestionID,
+        user,
+        Trip
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
@@ -138,7 +217,12 @@ export default {
       return updatedTrip;
     },
     addGeneralMessage: async (_, { tripID, message }, { Trip, user }) => {
-      const updatedTrip = await writeGeneralMessage(tripID, message, user, Trip);
+      const updatedTrip = await writeGeneralMessage(
+        tripID,
+        message,
+        user,
+        Trip
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
@@ -148,12 +232,22 @@ export default {
       return updatedTrip;
     },
     addTimeFrameMessage: async (_, { tripID, message }, { Trip, user }) => {
-      const updatedTrip = await writeTimeFrameMessage(tripID, message, user, Trip);
+      const updatedTrip = await writeTimeFrameMessage(
+        tripID,
+        message,
+        user,
+        Trip
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
     addDestinationMessage: async (_, { tripID, message }, { Trip, user }) => {
-      const updatedTrip = await writeDestinationMessage(tripID, message, user, Trip);
+      const updatedTrip = await writeDestinationMessage(
+        tripID,
+        message,
+        user,
+        Trip
+      );
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
       return updatedTrip;
     },
@@ -184,7 +278,10 @@ export default {
     leaveTrip: async (_, { tripID }, { Trip, user: { _id } }) => {
       const { updatedTrip, allTrips } = await leaveTrip(tripID, _id, Trip);
       pubsub.publish('TRIPINFO_CHANGED', { tripInfoChanged: updatedTrip });
-      pubsub.publish('OWN_TRIPS_CHANGED', { ownTripsChanged: allTrips, affectedUser: _id });
+      pubsub.publish('OWN_TRIPS_CHANGED', {
+        ownTripsChanged: allTrips,
+        affectedUser: _id
+      });
       return allTrips;
     }
   },
---
+++ tests/user.spec.js
@@ -0,0 +1,493 @@
+import axios from 'axios';
+import { XMLHttpRequest } from 'xmlhttprequest';
+
+global.XMLHttpRequest = XMLHttpRequest;
+
+describe('trip resolvers', () => {
+  test('create trip', async () => {
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `
+      mutation{
+        createTrip( trip: {
+          name:"Voyage des seniors dev",
+          participants:["berta@flock.io", "christopher@flock.io"],
+          destination:{
+            isDictated: true,
+            suggestions:[{name: "Palma de Mallorca"}]
+          },
+          budget: {suggestions: [{value: 100}]},
+        	timeFrame: {
+            isDictated: false,
+           suggestions: [
+            {
+              startDate: "2018-12-24",
+              endDate: "2018-12-25"
+            }
+          ]
+          }
+        })
+        {
+          name
+          participants{
+            email
+            firstName
+            lastName
+          }
+        }
+      }
+      `
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        createTrip: {
+          name: 'Voyage des seniors dev',
+          participants: [
+            {
+              email: 'christopher@flock.io',
+              firstName: 'Christopher',
+              lastName: 'Bücklein'
+            },
+            {
+              email: 'berta@flock.io',
+              firstName: 'Berta',
+              lastName: 'Cume'
+            }
+          ]
+        }
+      }
+    });
+  });
+
+  test('addOrVoteForBudget', async () => {
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `
+        mutation{
+          addOrVoteForBudget(tripID:  "000000000000000000000000", budget: {value: 100})
+          {
+            budget{
+              suggestions{
+               value
+              }
+            }
+          }
+        }
+      `
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        addOrVoteForBudget: {
+          budget: {
+            suggestions: [
+              {
+                value: 500
+              },
+              {
+                value: 100
+              }
+            ]
+          }
+        }
+      }
+    });
+  });
+
+  test('addOrVoteForTimeFrame', async () => {
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `
+        mutation{
+          addOrVoteForTimeFrame(tripID: "000000000000000000000000", timeFrames: {startDate: "2018-12-25", endDate: "2018-12-26"})
+      {
+        timeFrame{
+          suggestions{
+            voters{
+              email
+            }
+            startDate
+            endDate
+          }
+        }
+      }
+    }
+        `
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        addOrVoteForTimeFrame: {
+          timeFrame: {
+            suggestions: [
+              {
+                voters: [
+                  {
+                    email: 'christopher@flock.io'
+                  },
+                  {
+                    email: 'marco@flock.io'
+                  }
+                ],
+                startDate: '2018-12-16T00:00:00.000Z',
+                endDate: '2018-12-23T00:00:00.000Z'
+              },
+              {
+                voters: [
+                  {
+                    email: 'christopher@flock.io'
+                  },
+                  {
+                    email: 'marco@flock.io'
+                  }
+                ],
+                startDate: '2018-12-16T00:00:00.000Z',
+                endDate: '2018-12-23T00:00:00.000Z'
+              },
+              {
+                voters: [],
+                startDate: '2018-12-25T00:00:00.000Z',
+                endDate: '2018-12-26T00:00:00.000Z'
+              }
+            ]
+          }
+        }
+      }
+    });
+  });
+
+  test('addParticipants', async () => {
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `
+        mutation{
+          addParticipants(tripID:  "000000000000000000000000", participants: "barbara@flock.io")
+          {
+            participants{
+              email
+            }
+          }
+        }
+        `
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        addParticipants: {
+          participants: [
+            {
+              email: 'damien@flock.io'
+            },
+            {
+              email: 'christopher@flock.io'
+            },
+            {
+              email: 'berta@flock.io'
+            },
+            {
+              email: 'arturo@flock.io'
+            },
+            {
+              email: 'marco@flock.io'
+            },
+            {
+              email: 'barbara@flock.io'
+            }
+          ]
+        }
+      }
+    });
+  });
+
+  test('remove vote for destination', async () => {
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `mutation{
+          removeVoteForDestination(tripID:"000000000000000000000000", destinationID: "111111111111111111111111")
+        {
+          destination{
+            suggestions{
+              id
+              name
+              voters{
+                email
+                }
+              }
+            }
+        }
+      }`
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        removeVoteForDestination: {
+          destination: {
+            suggestions: [
+              {
+                id: '000000000000000000000000',
+                name: 'Barcelona',
+                voters: [
+                  {
+                    email: 'damien@flock.io'
+                  },
+                  {
+                    email: 'berta@flock.io'
+                  }
+                ]
+              },
+              {
+                id: '111111111111111111111111',
+                name: 'Berlin',
+                voters: [
+                  {
+                    email: 'damien@flock.io'
+                  },
+                  {
+                    email: 'berta@flock.io'
+                  }
+                ]
+              },
+              {
+                id: '222222222222222222222222',
+                name: 'Zurich',
+                voters: [
+                  {
+                    email: 'berta@flock.io'
+                  },
+                  {
+                    email: 'marco@flock.io'
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      }
+    });
+  });
+
+  test('add vote for destination', async () => {
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `
+        mutation{
+          addOrVoteForDestination(tripID:"000000000000000000000000", destinations: {name: "Zurich"})
+            {
+              destination{
+                suggestions{
+                name
+                voters{
+                  email
+                  }
+              }
+            }
+          }
+        }
+      `
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        addOrVoteForDestination: {
+          destination: {
+            suggestions: [
+              {
+                name: 'Barcelona',
+                voters: [
+                  {
+                    email: 'damien@flock.io'
+                  },
+                  {
+                    email: 'berta@flock.io'
+                  }
+                ]
+              },
+              {
+                name: 'Berlin',
+                voters: [
+                  {
+                    email: 'damien@flock.io'
+                  },
+                  {
+                    email: 'berta@flock.io'
+                  }
+                ]
+              },
+              {
+                name: 'Zurich',
+                voters: [
+                  {
+                    email: 'berta@flock.io'
+                  },
+                  {
+                    email: 'marco@flock.io'
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      }
+    });
+  });
+});
+
+describe('user resolvers', () => {
+  test('update user', async () => {
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `
+        mutation{
+          updateUser(id: "111111111111111111111111", update: {firstName: "Christopherausor"})
+          {
+            firstName
+            lastName
+          }
+        }
+      `
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        updateUser: {
+          firstName: 'Christopherausor',
+          lastName: 'Bücklein'
+        }
+      }
+    });
+  });
+
+  // test('allUsers', async () => {
+  //   const response = await axios.post('http://localhost:4000/graphql', {
+  //     query: `
+  //         {
+  //           allUsers {
+  //             firstName
+  //             lastName
+  //
+  //           }
+  //         }
+  //         `
+  //   });
+  //
+  //   const { data } = response;
+  //   expect(data).toMatchObject({
+  //     data: {
+  //       allUsers: [
+  //         {
+  //           firstName: 'Damien',
+  //           lastName: 'Derail'
+  //         },
+  //         {
+  //           firstName: 'Berta',
+  //           lastName: 'Cume'
+  //         },
+  //         {
+  //           firstName: 'Christopherausor',
+  //           lastName: 'Bücklein'
+  //         },
+  //         {
+  //           firstName: 'Arturo',
+  //           lastName: 'Moreira Santos'
+  //         },
+  //         {
+  //           firstName: 'Marco',
+  //           lastName: 'Kunz'
+  //         },
+  //         {
+  //           firstName: 'Damien1',
+  //           lastName: 'Derail'
+  //         },
+  //         {
+  //           firstName: 'Christopher1',
+  //           lastName: 'Bücklein'
+  //         },
+  //         {
+  //           firstName: 'Berta1',
+  //           lastName: 'Cume'
+  //         },
+  //         {
+  //           firstName: 'Arturo1',
+  //           lastName: 'Moreira Santos'
+  //         },
+  //         {
+  //           firstName: 'Marco1',
+  //           lastName: 'Kunz'
+  //         },
+  //         {
+  //           firstName: null,
+  //           lastName: null
+  //         }
+  //       ]
+  //     }
+  //   });
+  // });
+
+  //
+  // test('register user', async () => {
+  //   const response = await axios.post('http://localhost:4000/graphql', {
+  //     query: `
+  //       mutation {
+  //         register(email: "testuser@testuser.com", password: "tester", user: {firstName: "test", lastName: "user"})
+  //       }
+  // `
+  //   });
+  //
+  //   const { data } = response;
+  //
+  //   expect(data).toMatchObject({
+  //     data: {
+  //       register: {}
+  //     }
+  //   });
+  //
+  //   const response2 = await axios.post('http://localhost:4000/graphql', {
+  //     query: `
+  //     mutation {
+  //       login(email: "testuser@testuser.com", password: "tester")
+  //     }
+  //
+  //     `
+  //   });
+  //
+  //   const {
+  //     data: {
+  //       login: { token }
+  //     }
+  //   } = response2.data;
+  //
+  //   const response3 = await axios.post(
+  //     'http://localhost:4000/graphql',
+  //     {
+  //       query: `
+  //           {
+  //             self{
+  //               email
+  //               lastName
+  //               firstName
+  //             }
+  //           }
+  //           `
+  //     },
+  //     {
+  //       headers: {
+  //         'x-token': token
+  //         // 'x-token': token,
+  //         // 'x-refresh-token': refreshToken
+  //         // }
+  //       }
+  //     }
+  //   );
+  //   //
+  //   expect(response3.data).toMatchObject({
+  //     data: {
+  //       self: {
+  //         email: 'testuser@testuser.com',
+  //         lastName: 'user',
+  //         firstName: 'test'
+  //       }
+  //     }
+  //   });
+  // });
+});
