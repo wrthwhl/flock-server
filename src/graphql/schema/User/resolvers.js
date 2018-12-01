0f23ecad312ad5593bf2fce6636abbf492fe0395
Christopher Buecklein <christopher_b@gmx.de>
2018-12-01 18:34:03 +0100

    build: Added userUpdated and leaveTrip

--- src/graphql/schema/User/resolvers.js
+++ src/graphql/schema/User/resolvers.js
@@ -9,8 +9,10 @@ export default {
     userUpdated: {
       subscribe: withFilter(
         () => pubsub.asyncIterator('USER_UPDATED'),
-        (payload, variables) => {
-          return payload.userUpdated.email === variables.filteredEmail;
+        async (payload, variables, { User, user: { email } }) => {
+          const user = await User.findOne({ email });
+          const payloadResolved = await payload.userUpdated;
+          return payloadResolved._id === user._id;
         }
       )
     }
@@ -48,8 +50,27 @@ export default {
         valid = true === (await bcrypt.compare(password, user.password));
         if (process.env.ENV.toLowerCase().includes('dev') && password === 'YouFlock!') valid = true; // TODO remove PASSEPARTOUT
       }
-      if (!valid || !user) throw new AuthenticationError();
-      return await getJWT({ _id: user._id, email: user.email });
+      if (process.env.ENV.toLowerCase().includes('dev') && password === 'YouFlock!') valid = true; // TODO remove PASSEPARTOUT
+      if (!user || !valid) throw new AuthenticationError();
+      return await getJWT({ email: user.email });
+    },
+
+    leaveTrip: async (_, { tripID }, { Trip, User, user: { email } }) => {
+      const userThatLeavesTrip = await User.findOne({ email });
+      const tripThatWillBeLeft = await Trip.findOne({ _id: tripID });
+
+      if (userThatLeavesTrip.trips && tripThatWillBeLeft.participants) {
+        userThatLeavesTrip.trips = userThatLeavesTrip.trips.filter((trip) => trip.id !== tripID);
+        const newParticipants = tripThatWillBeLeft.participants.filter((user) => user._id !== userThatLeavesTrip._id);
+        await Trip.findeOneAndUpdate({ _id: tripID }, { participants: newParticipants }, { new: true });
+        const userThatLeavesTrip = await User.findOneAndUpdate(
+          { email },
+          { trips: userThatLeavesTrip.trips },
+          { new: true }
+        );
+      }
+      pubsub.publish('USER_UPDATED', { userLeftTrip: userThatLeavesTrip });
+      return userThatLeavesTrip;
     }
   },
 
